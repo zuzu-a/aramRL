864dfc6ff76bf2107f6e4d8b684800476677c213
zuzu-a <halsuwai@purdue.edu>
2025-01-09 01:54:59 -0500

    Continuation of rebase.

--- inc/rogue.hpp
+++ inc/rogue.hpp
@@ -26,25 +26,13 @@ typedef struct {
     char avatar;
 } Entity;
 
+// Screen size
+#define SCREEN_WIDTH 46
+#define SCREEN_HEIGHT 20
 
-// player.c functions
-Entity* createPlayer(Position start_pos);
-void handleInput(int input);
-
-// externs
-extern Entity *player;
-
-/* sample screen size */
-#define SAMPLE_SCREEN_WIDTH 46
-#define SAMPLE_SCREEN_HEIGHT 20
-/* sample screen position */
+// Screen position
 #define SAMPLE_SCREEN_X 20
 #define SAMPLE_SCREEN_Y 10
-
-// A custom SDL event for when a sample is switched to.
-#define ON_ENTER_USEREVENT (SDL_USEREVENT + 0)
-// A custom SDL event to tell a sample to draw.
-#define ON_DRAW_USEREVENT (SDL_USEREVENT + 1)
 
 static float delta_time = 0.0f;  // The time in seconds of the current frame.
 #define DELTA_SAMPLES_LENGTH 64
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <rogue.h>
+#include <rogue.hpp>
 
 int main(int argc, char* argv[]) {
   
